20c1ccc5f566fcf6b713f006ab4b4b369245f8ab
Alexander Murphy <amurphy345@gmail.com>
2017-11-19 23:08:56 -0700

    Initial Commit

---
+++ nav_bar_color_transition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nav_bar_color_transition
+//
+//  Created by Alexander Murphy on 11/19/17.
+//  Copyright © 2017 Alexander Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
